34d1a7c418ce940b9517e400c604a79d67010082
RaduTudorIon <radutudorion@gmail.com>
2019-01-29 17:23:21 +0200

    Data layer

---
+++ SimpleLibraryManagement/Data/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,15 @@
+﻿using SimpleLibraryManagement.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleLibraryManagement.Data.Interfaces
+{
+    public interface IAuthorRepository
+    {
+        IEnumerable<Author> GetAllWithBooks();
+
+        Author GetWithBook(int id);
+    }
+}
---
+++ SimpleLibraryManagement/Data/Interfaces/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using SimpleLibraryManagement.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleLibraryManagement.Data.Interfaces
+{
+    public interface BookRepository 
+    {
+         IEnumerable<Book> FindWithAuthor(Func<Book, bool> predicate);
+
+         IEnumerable<Book> FindWithAuthorAndBorrower(Func<Book, bool> predicate);
+
+         IEnumerable<Book> GetAllWithAuthor();
+    }
+}
