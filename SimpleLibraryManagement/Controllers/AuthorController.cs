c50e0152b649ddf9b1cb2c89cdc1b3d589fc793b
RaduTudorIon <radutudorion@gmail.com>
2019-02-13 18:59:19 +0200

    20 including

---
+++ SimpleLibraryManagement/Controllers/AuthorController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleLibraryManagement.Data.Interfaces;
+using SimpleLibraryManagement.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleLibraryManagement.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorController(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        [Route("Author")]
+        public IActionResult List()
+        {
+            var authors = _authorRepository.GetAllWithBooks();
+
+            if (authors.Count() == 0 || authors ==null)
+                return View("Empty");
+
+            return View(authors);
+        }
+
+        public IActionResult Update(int id)
+        {
+            var author = _authorRepository.GetById(id);
+
+            if (author == null) return NotFound();
+
+            return View(author);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Author author)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(author);
+            }
+
+            _authorRepository.Update(author);
+
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Author author)
+        {
+            _authorRepository.Create(author);
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var author = _authorRepository.GetById(id);
+
+            _authorRepository.Delete(author);
+
+            return RedirectToAction("List");
+        }
+    }
+}
