1c475a77ffc9a249143fa1b7fde9e8c78767df50
RaduTudorIon <radutudorion@gmail.com>
2019-02-22 18:29:55 +0200

    27 (including)

--- SimpleLibraryManagement/Controllers/BookController.cs
+++ SimpleLibraryManagement/Controllers/BookController.cs
@@ -20,6 +20,7 @@ namespace SimpleLibraryManagement.Controllers
             _authorRepository = authorRepository;
         }
 
+        [Route("Book")]
         public IActionResult List(int? authorId, int? borrowerId)
         {
             if(authorId == null && borrowerId == null)
